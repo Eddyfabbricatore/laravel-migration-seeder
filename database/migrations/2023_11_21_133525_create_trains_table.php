3a56999df0850c663f10019a21cc22815640a224
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-11-21 16:01:34 +0100

    [add] migrations

---
+++ database/migrations/2023_11_21_133525_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->date('departure_time');
+            $table->date('arrival_time');
+            $table->string('train_code');
+            $table->integer('number_carriages');
+            $table->boolean('is_in_time');
+            $table->boolean('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
